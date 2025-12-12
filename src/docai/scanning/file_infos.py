bd63d6e26e273d743d437d849c6bc875d0fbdfa8
Quentin Vitt <quentinvitt@icloud.com>
2025-12-12 02:43:35 +0100

    logging for func get_file_type finished

--- src/docai/scanning/file_infos.py
+++ src/docai/scanning/file_infos.py
@@ -1,25 +1,30 @@
 import logging
 import os
+from threading import Lock
 
 import filetype
 
+logger = logging.getLogger("docai_project")
+
 
 def get_file_type(file: str) -> str:
-    # TODO: logging
-
     # check if this is a real file
     if not os.path.isfile(file):
+        logger.error(f"File '{file}' does not exist")
         raise FileNotFoundError(f"File '{file}' does not exist")
 
     # check for extention
     _, file_extension = os.path.splitext(file)
     if file_extension != "":
+        logger.debug(f"File type of '{file}' identified via extension")
         return file_extension[1:]
 
     # check for magic numbers
     kind = filetype.guess(file)
     if kind is not None:
+        logger.debug(f"File type of '{file}' identified via magic numbers")
         return kind.extension
 
     # if file type could not be determined return None
+    logger.debug(f"File type of '{file}' could not be determined")
     return None
