9ef28440b7555afd9883141fcd3da8e0a19034f4
Quentin Vitt <quentinvitt@icloud.com>
2025-12-12 14:26:28 +0100

    first steps for universal_extractor setup

--- src/docai/scanning/file_infos.py
+++ src/docai/scanning/file_infos.py
@@ -7,24 +7,24 @@ import filetype
 logger = logging.getLogger("docai_project")
 
 
-def get_file_type(file: str) -> str:
+def get_file_type(file: str) -> str | None:
     # check if this is a real file
     if not os.path.isfile(file):
-        logger.error(f"File '{file}' does not exist")
+        logger.error("File '%s' does not exist", file, exc_info=True)
         raise FileNotFoundError(f"File '{file}' does not exist")
 
     # check for extention
     _, file_extension = os.path.splitext(file)
     if file_extension != "":
-        logger.debug(f"File type of '{file}' identified via extension")
+        logger.debug("File type of '%s' identified via extension", file)
         return file_extension[1:]
 
     # check for magic numbers
     kind = filetype.guess(file)
     if kind is not None:
-        logger.debug(f"File type of '{file}' identified via magic numbers")
+        logger.debug("File type of '%s' identified via magic numbers", file)
         return kind.extension
 
     # if file type could not be determined return None
-    logger.debug(f"File type of '{file}' could not be determined")
+    logger.debug("File type of '%s' could not be determined", file)
     return None
